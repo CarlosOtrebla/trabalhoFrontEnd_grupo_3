e771ce0223e07d8f0c89cd84186bdea966e6d58a
adinor <adinor_original@hotmail.com>
2024-05-03 18:00:55 -0300

    ultimas alterações

--- Trabalho_final/scripts/Carrinho.js
+++ Trabalho_final/scripts/Carrinho.js
@@ -3,29 +3,38 @@ console.log('itens recebidos: ', carrinho)
 
 function carregarItensDoCarrinho(carrinho) {
     const cards = document.querySelector('tbody');
-    const totalValor = 0.0 //servindo para operação, mas ainda não utilziada.
-    
+    const totalElement = document.getElementById('total');
+    let totalValor = 0.0;
+
     cards.innerHTML = '';
 
     carrinho.forEach(item => {
         const qtd = parseInt(item.quantidade);
+        const total = parseFloat(item.quantidade * item.preco)
         const itemHTML = `
-            <div class="item-do-carrinho">
-                <img class="imagem" src="${item.imagem}" alt="${item.titulo}">
-                <div class="nome" >${item.titulo}</div>
-                <div class="preco" >R$ ${item.preco.toFixed(2)}</div>
-                <label for="quantidade-${item.titulo}">Quantidade:</label>
-                <input type="number" id="quantidade-${item.titulo}" name="quantidade-${item.titulo}" value="${qtd}" min="1">
-            </div>
+            <tr class="item-do-carrinho">
+                <td>
+                    <img class="imagem" src="${item.imagem}" alt="${item.titulo}">
+                    <div class="nome">${item.titulo}</div>
+                </td>
+                <td class="preco">R$ ${item.preco.toFixed(2)}</td>
+                <td>
+                    <label for="quantidade-${item.titulo}">Quantidade:</label>
+                    <input type="number" id="quantidade-${item.titulo}" name="quantidade-${item.titulo}" value="${qtd}" min="1">
+                </td>
+            </tr>
         `;
+        totalValor += total;
         cards.innerHTML += itemHTML;
     });
+
+    totalElement.textContent = `Total: R$ ${totalValor.toFixed(2)}`;
 }
+
 document.addEventListener('DOMContentLoaded', function() {
     if (carrinho.length > 0) {
         carregarItensDoCarrinho(carrinho);
     } else {
+        // Código para lidar com o carrinho vazio, se necessário
     }
-
-}
-)
+});
